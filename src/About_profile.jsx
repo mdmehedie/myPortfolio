7cacfb64c04eb25c5b0326ac09fc80a610ecd011
Mehedi Hasan <mehedicse04@gmail.com>
2021-03-22 00:37:02 +0600

    Modified About Section

--- src/About_profile.jsx
+++ src/About_profile.jsx
@@ -1,21 +1,27 @@
 import React from "react";
-import "./About_profile";
+import "./About_profile.css";
+import profile_pic from "./img/pngaaa.com-3873853.png";
 
 function About_profile() {
   return (
-    <div className="about">
-      <div className="container">
-        <div>
-          <img src="./img/pngaaa.com-3873853" alt="" />
+    <div
+      className="d-flex justify-content-center align-items-center"
+      height="100vh"
+      width="100%"
+    >
+      <div className=" container d-flex justify-content-center align-items-center ">
+        <div className="about__image ">
+          <img src={profile_pic} alt="" className="px-5" />
         </div>
-        <div >
+        <div>
           <h1>About Me</h1>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged.
+          remaining essentially unchanged.when an unknown printer took a galley
+          of type and scrambled it to make a type specimen book.
         </div>
       </div>
     </div>
